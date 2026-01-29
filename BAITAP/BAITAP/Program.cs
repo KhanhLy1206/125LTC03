1e39684dc9301deb4815783c6d338272c8771037
KhanhLy1206 <khanhly12062005@gmail.com>
2026-01-29 10:33:32 +0700

    xong bai tap

--- BAITAP/BAITAP/Program.cs
+++ BAITAP/BAITAP/Program.cs
@@ -41,9 +41,131 @@ namespace BAITAP
                 Console.WriteLine("Bai 3: Chuỗi là đối xứng");
             else
                 Console.WriteLine("Bai 3: Chuỗi không đối xứng");
+            Bai4_5_6();
+            Bai7();
+            Bai8();
+            Bai9();
 
         }
 
+        static void Bai4_5_6()
+        {
+            Console.WriteLine("Bai 4,5,6: Nhap so phan tu: ");
+            int spt = int.Parse(Console.ReadLine());
+            int[] arr = new int[spt];
+            Console.WriteLine("Nhap cac phan tu: ");
+            for (int i = 0; i < spt; i++)
+            {
+                Console.WriteLine($"arr[{i}]= ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            if (arr == null)
+            {
+                Console.WriteLine("Mang chua khoi tao");
+                return;
+            }
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("Bai 4: Tong cac phan tu mang la: " + sum);
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            Console.WriteLine("Bai 5: Max = " + max);
+            int demChan = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 2 == 0)
+                    demChan++;
+            Console.WriteLine("Bai 6: So phan tu chan trong mang la: " + demChan);
+        }
+        static void Bai7()
+        {
+            Console.WriteLine("Bai 7: Nhap chuoi");
+            String chuoi = Console.ReadLine();
+            if (chuoi == null)
+            {
+                Console.WriteLine("Chuoi null-khong tach");
+                return;
+            }
+            string[] arrName = chuoi.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
+            if (arrName == null)
+            {
+                Console.WriteLine("Mảng kết quả null - không in!");
+                return;
+            }
+
+            Console.WriteLine("Bai 7 - Các tu trong ho ten:");
+
+            for (int i = 0; i < arrName.Length; i++)
+                Console.WriteLine(arrName[i]);
+
+            Console.WriteLine();
+
+
+        }
+        static void Bai8()
+        {
+            Console.WriteLine("Bai 8: Nhap vao cau: ");
+            String sentence = Console.ReadLine();
+            if (sentence == null)
+            {
+                Console.WriteLine("Chuoi null");
+                return;
+            }
+            string[] words = sentence.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 0)
+            {
+                Console.WriteLine("Không có từ!");
+                return;
+            }
+
+            string longest = words[0];
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length > longest.Length)
+                    longest = words[i];
+            }
+
+            Console.WriteLine("Ket qua bai 8: Tu dai nhat la: " + longest);
+            Console.WriteLine();
+        }
+
+        static void Bai9()
+        {
+            Console.Write("Bai 9: Nhap chuoi: ");
+            string input = Console.ReadLine();
+
+            if (input == null)
+            {
+                Console.WriteLine("Chuỗi null - không xử lý!");
+                return;
+            }
+
+            string[] arr = input.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (arr == null)
+            {
+                Console.WriteLine("Mảng null - không xử lý!");
+                return;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(arr[i]))
+                    count++;
+            }
+
+            Console.WriteLine("Ket qua bai 9: So chuoi khac null va khac rong la: " + count);
+        }
     }
 }
--- BAITAP/BAITAP/obj/Debug/net8.0/BAITAP.AssemblyInfo.cs
+++ BAITAP/BAITAP/obj/Debug/net8.0/BAITAP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BAITAP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137e680e980ac58b18ba6812f152beff841f2322")]
 [assembly: System.Reflection.AssemblyProductAttribute("BAITAP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BAITAP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
