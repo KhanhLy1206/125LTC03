efe93d8a4a4a83c765b97581b17fd5330ee4156d
KhanhLy1206 <khanhly12062005@gmail.com>
2026-04-10 21:59:42 +0700

    Tao model/class Student

---
+++ MVC_1/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
