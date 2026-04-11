361ceb4a25da4e374d0e1be7ac9cb241d7a63d74
KhanhLy1206 <khanhly12062005@gmail.com>
2026-04-11 21:46:16 +0700

    Tao models gom Product.cs va DbfirstDbContext.cs

---
+++ DBFIRST/DBFIRST/Models/DbfirstDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFIRST.Models;
+
+public partial class DbfirstDbContext : DbContext
+{
+    public DbfirstDbContext()
+    {
+    }
+
+    public DbfirstDbContext(DbContextOptions<DbfirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=DBFirstDB;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC27AA77C863");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Price).HasColumnType("money");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(255)
+                .HasColumnName("Product_Name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DBFIRST/DBFIRST/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFIRST.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public decimal? Price { get; set; }
+}
