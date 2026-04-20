2df26701d7e1d4b48d75233989ac376a81441361
KhanhLy1206 <khanhly12062005@gmail.com>
2026-04-20 21:41:49 +0700

    Create AppDbContext.cs

---
+++ BaiTap_MVC/BaiTap_MVC/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaiTap_MVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<SinhVien> SinhViens { get; set; }
+        public DbSet<GiaoVien> GiaoViens { get; set; }
+        public DbSet<KhoaHoc> KhoaHocs { get; set; }
+        public DbSet<LopHocPhan> LopHocPhans { get; set; }
+        public DbSet<DangKyHoc> DangKyHocs { get; set; }
+    }
+}
