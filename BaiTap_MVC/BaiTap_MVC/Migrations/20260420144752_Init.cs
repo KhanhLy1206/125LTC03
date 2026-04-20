af8296f25f3629bda3cb69906f8d0339c9fb9e3e
KhanhLy1206 <khanhly12062005@gmail.com>
2026-04-20 21:49:00 +0700

    add migration

---
+++ BaiTap_MVC/BaiTap_MVC/Migrations/20260420144752_Init.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiTap_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GiaoViens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GiaoViens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KhoaHocs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenKhoaHoc = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KhoaHocs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SinhViens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tuoi = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SinhViens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LopHocPhans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenLopHocPhan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KhoaHocId = table.Column<int>(type: "int", nullable: false),
+                    GiaoVienId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LopHocPhans", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LopHocPhans_GiaoViens_GiaoVienId",
+                        column: x => x.GiaoVienId,
+                        principalTable: "GiaoViens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LopHocPhans_KhoaHocs_KhoaHocId",
+                        column: x => x.KhoaHocId,
+                        principalTable: "KhoaHocs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DangKyHocs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SinhVienId = table.Column<int>(type: "int", nullable: false),
+                    LopHocPhanId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DangKyHocs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DangKyHocs_LopHocPhans_LopHocPhanId",
+                        column: x => x.LopHocPhanId,
+                        principalTable: "LopHocPhans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DangKyHocs_SinhViens_SinhVienId",
+                        column: x => x.SinhVienId,
+                        principalTable: "SinhViens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DangKyHocs_LopHocPhanId",
+                table: "DangKyHocs",
+                column: "LopHocPhanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DangKyHocs_SinhVienId",
+                table: "DangKyHocs",
+                column: "SinhVienId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LopHocPhans_GiaoVienId",
+                table: "LopHocPhans",
+                column: "GiaoVienId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LopHocPhans_KhoaHocId",
+                table: "LopHocPhans",
+                column: "KhoaHocId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DangKyHocs");
+
+            migrationBuilder.DropTable(
+                name: "LopHocPhans");
+
+            migrationBuilder.DropTable(
+                name: "SinhViens");
+
+            migrationBuilder.DropTable(
+                name: "GiaoViens");
+
+            migrationBuilder.DropTable(
+                name: "KhoaHocs");
+        }
+    }
+}
